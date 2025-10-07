958b7d4530deee1e40321689a86b1998ff05835b
krishnapriya-30 <kichukrishnapriya192@gmail.com>
2025-10-07 15:44:21 +0530

    Add files via upload

---
+++ arealambda.py
@@ -0,0 +1,20 @@
+a=float(input("Enter the length:"))
+b=float(input("Enter the width:"))
+rect=lambda a,b:a*b
+print("Area of ractangle using lambda function:",rect(a,b))
+s=float(input("Enter the value:"))
+side=lambda s:s*s
+print("Area of square using lambda function:",side(s))
+c=float(input("Enter the base:"))
+d=float(input("Enter the height:"))
+tri=lambda c,d:1/2*c*d
+print("Area of triangle using lambda function:",tri(c,d))
+
+
+
+
+
+
+
+
+
